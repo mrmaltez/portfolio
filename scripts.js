424cf5e12673b01df0b2184d29240db3026cfb7f
Matheus Maltez <mrmaltez@gmail.com>
2023-04-21 06:47:19 -0300

    primeiro commit do meu projeto pessoal do portfolio

---
+++ scripts.js
@@ -0,0 +1,45 @@
+const carousel = document.querySelector('.carousel');
+const carouselContainer = carousel.querySelector('.carousel-container');
+const carouselItems = carousel.querySelectorAll('.carousel-item');
+const carouselPrev = carousel.querySelector('.carousel-prev');
+const carouselNext = carousel.querySelector('.carousel-next');
+const carouselItemWidth = carouselItems[0].offsetWidth;
+let currentIndex = 0;
+
+carouselPrev.addEventListener('click', (event) => {
+  event.preventDefault();
+  currentIndex = currentIndex > 0 ? currentIndex - 1 : carouselItems.length - 1;
+  carouselContainer.style.transform = `translateX(-${currentIndex * carouselItemWidth}px)`;
+});
+
+carouselNext.addEventListener('click', (event) => {
+  event.preventDefault();
+  currentIndex = currentIndex < carouselItems.length - 1 ? currentIndex + 1 : 0;
+  carouselContainer.style.transform = `translateX(-${currentIndex * carouselItemWidth}px)`;
+});
+
+
+// Definindo a função que irá mudar os slides
+function slide() {
+  const firstItem = carousel.querySelector('.carousel-item:first-child');
+  const lastItem = carousel.querySelector('.carousel-item:last-child');
+  const cloneFirstItem = firstItem.cloneNode(true);
+  
+  // Adicionando a classe que irá fazer a transição do slide
+  carouselContainer.classList.add('transition');
+  
+  // Movendo o carrossel para a esquerda
+  carouselContainer.style.transform = `translateX(-${firstItem.offsetWidth}px)`;
+  
+  // Quando a transição terminar, remova o primeiro item e adicione-o como clone do último item
+  setTimeout(() => {
+    firstItem.remove();
+    carouselContainer.appendChild(cloneFirstItem);
+    carouselContainer.classList.remove('transition');
+    carouselContainer.style.transform = 'translateX(0)';
+  }, 300);
+}
+
+// Definindo o intervalo de tempo para mudar os slides (5 segundos)
+setInterval(slide, 3000);
+
